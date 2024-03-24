bbe448630ecac3bd2f1ea2b7ef68f0ffbe3a1008
theyoostink <theyoostink@gmail.com>
2024-03-24 19:37:59 -0400

    Initial commit with placeholder images

---
+++ README.md
@@ -0,0 +1,5 @@
+# commissions
+
+## About
+
+This is a webpage for my art commissions. Check it out! [https://talkingdinosaur.github.io/commissions/](https://talkingdinosaur.github.io/commissions/)
---
+++ js/data.js
@@ -0,0 +1,124 @@
+var data = {
+	images: [
+		{
+			src: [""],
+			thumbnail: "",
+			title: "Placeholder Entry",
+			artist: "Placeholder Entry",
+			artist_url: null,
+			art_url: null,
+			desc: "Placeholder Entry",
+			date_str: "Beginning of Time",
+			tags: [""],
+			hidden: true
+		},
+		{
+			src: ["https://cdn.imgchest.com/files/myd5cl6a294.png"],
+			thumbnail: "https://cdn.imgchest.com/files/myd5cl6a294.png",
+			title: "a",
+			artist: "",
+			artist_url: "",
+			art_url: "",
+			desc: "",
+			date_str: "",
+			tags: ["fire_emblem", "larcei", "sfw"],
+			hidden: false
+		},
+		{
+			src: ["https://cdn.imgchest.com/files/g4z9cx56ep7.png"],
+			thumbnail: "https://cdn.imgchest.com/files/g4z9cx56ep7.png",
+			title: "b",
+			artist: "",
+			artist_url: "",
+			art_url: "",
+			desc: "",
+			date_str: "",
+			tags: ["fire_emblem", "seliph", "nsfw"],
+			hidden: false
+		},
+		{
+			src: ["https://cdn.imgchest.com/files/84jdc95lxn4.jpg"],
+			thumbnail: "https://cdn.imgchest.com/files/84jdc95lxn4.jpg",
+			title: "c",
+			artist: "",
+			artist_url: "",
+			art_url: "",
+			desc: "",
+			date_str: "",
+			tags: ["fire_emblem", "larcei", "seliph", "sfw"],
+			hidden: false
+		},
+		{
+			src: ["https://cdn.imgchest.com/files/pyq9cn9wkm4.jpg"],
+			thumbnail: "https://cdn.imgchest.com/files/pyq9cn9wkm4.jpg",
+			title: "d",
+			artist: "",
+			artist_url: "",
+			art_url: "",
+			desc: "",
+			date_str: "",
+			tags: ["fire_emblem", "larcei", "sfw"],
+			hidden: false
+		},
+		{
+			src: ["https://cdn.imgchest.com/files/l4necvdpgg4.png"],
+			thumbnail: "https://cdn.imgchest.com/files/l4necvdpgg4.png",
+			title: "e",
+			artist: "",
+			artist_url: "",
+			art_url: "",
+			desc: "",
+			date_str: "",
+			tags: ["fire_emblem", "lana", "patty", "sfw"],
+			hidden: false
+		},
+		{
+			src: ["https://cdn.imgchest.com/files/w7w6cod3wzy.jpg"],
+			thumbnail: "https://cdn.imgchest.com/files/w7w6cod3wzy.jpg",
+			title: "f",
+			artist: "",
+			artist_url: "",
+			art_url: "",
+			desc: "",
+			date_str: "",
+			tags: ["fire_emblem", "larcei", "seliph", "nsfw"],
+			hidden: false
+		},
+		{
+			src: ["https://cdn.imgchest.com/files/j7kzc3qjp97.jpg"],
+			thumbnail: "https://cdn.imgchest.com/files/j7kzc3qjp97.jpg",
+			title: "g",
+			artist: "",
+			artist_url: "",
+			art_url: "",
+			desc: "",
+			date_str: "",
+			tags: ["fire_emblem", "larcei", "nsfw"],
+			hidden: false
+		},
+		{
+			src: ["https://cdn.imgchest.com/files/l7lxc28qaz7.jpg"],
+			thumbnail: "https://cdn.imgchest.com/files/l7lxc28qaz7.jpg",
+			title: "h",
+			artist: "",
+			artist_url: "",
+			art_url: "",
+			desc: "",
+			date_str: "",
+			tags: ["fire_emblem", "larcei", "seliph", "nsfw"],
+			hidden: false
+		},
+		{
+			src: [""],
+			thumbnail: "",
+			title: "",
+			artist: "",
+			artist_url: "",
+			art_url: "",
+			desc: "",
+			date_str: "",
+			tags: [""],
+			hidden: true
+		}
+	]
+};

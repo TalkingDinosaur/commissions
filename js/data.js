19a6f2949573ebb4bd21f2dbfa2f1f16b084ec37
TalkingDinosaur <162406313+TalkingDinosaur@users.noreply.github.com>
2024-05-18 01:47:48 -0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1093,6 +1093,54 @@ var data = {
 			hidden: false
 		},
 		{
+			src: ["https://cdn.imgchest.com/files/j7kzcx8ojd7.png", "https://cdn.imgchest.com/files/d7ogcxpqmby.png"],
+			thumbnail: "https://cdn.imgchest.com/files/j7mmcnglkn7.png",
+			title: "Larcei congratulates Seliph on his resplendent",
+			artist: "Decox",
+			artist_url: "https://x.com/decoxart",
+			art_url: "https://x.com/decoxart/status/1509244977578229762",
+			desc: "",
+			date_str: "March 2022",
+			tags: ["fire_emblem", "larcei", "seliph", "nsfw"],
+			hidden: false
+		},
+		{
+			src: ["https://cdn.imgchest.com/files/p7bwc2jvel7.png", "https://cdn.imgchest.com/files/l4neclojnp4.png"],
+			thumbnail: "https://cdn.imgchest.com/files/l4neclojre4.png",
+			title: "Patty gets caught \(and is offered a trade~\) 1",
+			artist: "Decox",
+			artist_url: "https://x.com/decoxart",
+			art_url: "https://x.com/decoxart/status/1521298896240234497",
+			desc: "",
+			date_str: "May 2022",
+			tags: ["fire_emblem", "patty", "nsfw"],
+			hidden: false
+		},
+		{
+			src: ["https://cdn.imgchest.com/files/w7pjckm8dl7.png"],
+			thumbnail: "https://cdn.imgchest.com/files/6yxkcqxm3z7.png",
+			title: "Cowgirl Larcei 1",
+			artist: "Decox",
+			artist_url: "https://x.com/decoxart",
+			art_url: "https://twitter.com/Rs40Uchiha/status/1532767818395025413",
+			desc: "",
+			date_str: "May 2022",
+			tags: ["fire_emblem", "larcei", "seliph", "nsfw"],
+			hidden: false
+		},
+		{
+			src: ["https://cdn.imgchest.com/files/p7bwc2jvpn7.png", "https://cdn.imgchest.com/files/l4neclojqr4.png"],
+			thumbnail: "https://cdn.imgchest.com/files/w7w6cqng83y.png",
+			title: "Cowgirl Larcei 2",
+			artist: "Decox",
+			artist_url: "https://x.com/decoxart",
+			art_url: "https://x.com/decoxart/status/1553192260988174342",
+			desc: "",
+			date_str: "July 2022",
+			tags: ["fire_emblem", "larcei", "seliph", "nsfw"],
+			hidden: false
+		},
+		{
 			src: [""],
 			thumbnail: "",
 			title: "",
